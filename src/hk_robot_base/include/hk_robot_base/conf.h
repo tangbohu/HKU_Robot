7c7a34ebe409407eb54c7cee3c22b269dacb5205
KK Tang <kktang@mail.ustc.edu.cn>
2017-09-06 15:48:47 +0800

    Initial commit

---
+++ src/hk_robot_base/include/hk_robot_base/conf.h
@@ -0,0 +1,9 @@
+#ifndef CONF_H
+#define CONF_H
+
+string TurtbleBotSlamNodeName="ss";
+string TurbleBotSlamTopicName="ss";
+string TurtbleBotSlamCmdNodeName ="ss";
+string TurtbleBotSlamCmdTopicName ="ss";
+
+#endif // CONF_H
