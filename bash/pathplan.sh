1b28f7d93849b0e28a1f0beb2a28672fc6253ee3
ACThunder <805861343@qq.com>
2017-10-22 15:14:57 +0800

    add some bash files

--- bash/pathplan.sh
+++
@@ -1,1 +0,0 @@
-rosrun hku_robot_base hku_robot_base ~/HKU_Robot/data/mapdata/nav_poses401passage.txt
--- bash/setup.sh
+++ bash/setup.sh
@@ -1,5 +1,4 @@
-gnome-terminal --tab -e  "bash -c \"sleep .4;rosrun master_discovery_fkie master_discovery;bash\""  --tab -e  "bash -c \"sleep .4;roslaunch master_sync_fkie msync.launch;bash\""       --tab -e "bash -c \"source ~/HKU_Robot/hku_robot_ws/devel/setup.bash; roslaunch rtabmap_ros turtlebot_launch.launch; bash\""  --tab -e "bash -c \"source ~/HKU_Robot/hku_robot_ws/devel/setup.bash; sleep 5; . amcl_nav.sh; bash\""  
-
+gnome-terminal --tab -e  "bash -c \"source ~/catkin_ws/devel/setup.bash;sleep .2;rosrun master_discovery_fkie master_discovery;bash\""  --tab -e  "bash -c \"source ~/catkin_ws/devel/setup.bash;sleep .2;roslaunch master_sync_fkie msync.launch;bash\""       --tab -e "bash -c \"source ~/HKU_Robot/hku_robot_ws/devel/setup.bash; roslaunch rtabmap_ros turtlebot_launch.launch; bash\""  --tab -e "bash -c \"source ~/HKU_Robot/hku_robot_ws/devel/setup.bash; sleep 3; . amcl_nav.sh; bash\""  --tab -e "bash -c \"sleep 3;roslaunch turtlebot_rviz_launchers view_navigation.launch --screen;bash\""
 
 
 
